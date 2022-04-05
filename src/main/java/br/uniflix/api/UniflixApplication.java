9daa816391015cfbf5b7aa5c5e303cfc4448ef6f
Lucas Lins <lucas11lins@gmail.com>
2022-04-05 20:36:09 -0300

    Adicionado projeto base

---
+++ src/main/java/br/uniflix/api/UniflixApplication.java
@@ -0,0 +1,13 @@
+package br.uniflix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniflixApplication.class, args);
+    }
+
+}
