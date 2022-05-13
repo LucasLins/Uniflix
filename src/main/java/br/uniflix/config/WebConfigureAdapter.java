cc384349b28dd464a138b648c40fa443fbb12d5a
Thalyson Rocha <thalysonufpb@gmail.com>
2022-05-13 20:20:45 -0300

    Adição de arquivos de Configuração
    .

---
+++ src/main/java/br/uniflix/config/WebConfigureAdapter.java
@@ -0,0 +1,21 @@
+package br.uniflix.config;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.validation.constraints.NotNull;
+
+@EnableWebMvc
+public class WebConfigureAdapter implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(@NotNull ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
